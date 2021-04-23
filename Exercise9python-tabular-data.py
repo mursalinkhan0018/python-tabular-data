e87d8468e2cbe193b8689f79e8e40d8c8feeb7ef
MD Mursalin Khan <mursalinkhan0018@gmail.com>
2021-04-22 22:24:41 -0500

    Ignore the alternative parts

--- Exercise9python-tabular-data.py
+++ Exercise9python-tabular-data.py
@@ -1,11 +1,74 @@
 #! /usr/bin/env python3
-#new one 
+
 "A script to perform the linear regression and create the plot."
 
 import sys
 import pandas as pd
 import matplotlib.pyplot as plt
 from scipy import stats
+
+dataframe = pd.read_csv(sys.argv[1], error_bad_lines=False)
+groups ="species"
+x_col="petal_length_cm"
+y_col="sepal_length_cm"
+
+if groups:
+	grouped_dataframes = dataframe.groupby(groups)
+else:
+	grouped_dataframes = ('all', dataframe),
+
+color_index = 1
+for category, df in grouped_dataframes:
+    x = df[x_col]
+    y = df[y_col]
+    regression = stats.linregress(x, y)
+    slope = regression.slope
+    intercept = regression.intercept
+    plt.scatter(x, y, label = category, color = 'C' + str(color_index))
+    plt.plot(x,slope *x +intercept, color = 'C' +str(color_index),label = category)
+    color_index += 1
+plt.xlabel(x_col)
+plt.ylabel(y_col)
+plt.legend()
+plt.savefig("XXXXX-Petal vs sepal length.png")
+
+quit()
+
+
+
+#alternative ways 
+
+x1 = Iris_virginica.petal_length_cm
+y1 = Iris_virginica.sepal_length_cm
+regression = stats.linregress(x1, y1)
+slope = regression.slope
+intercept = regression.intercept
+plt.scatter(x1, y1, label = 'Iris_virginica')
+plt.plot(x1, slope * x1 + intercept, color = "orange", label = 'Iris_virginica line')
+
+x2 = Iris_setosa.petal_length_cm
+y2 = Iris_setosa.sepal_length_cm
+regression = stats.linregress(x2, y2)
+slope = regression.slope
+intercept = regression.intercept
+plt.scatter(x2, y2, label = 'Iris_setosa')
+plt.plot(x2, slope * x2 + intercept, color = "red", label = 'Iris_setosa line')
+
+x3 = Iris_versicolor.petal_length_cm
+y3 = Iris_versicolor.sepal_length_cm
+regression = stats.linregress(x3, y3)
+slope = regression.slope
+intercept = regression.intercept
+plt.scatter(x3, y3, label = 'Iris_versicolor')
+plt.plot(x3, slope * x3 + intercept, color = "blue", label = 'Iris_versicolor line')
+
+plt.xlabel("Petal length (cm)")
+plt.ylabel("Sepal length (cm)")
+plt.legend()
+plt.savefig("AA-Iris_virginica petal vs sepal length.png")
+quit()
+
+#Three plots in one figures
 
 dataframe = pd.read_csv(sys.argv[1], error_bad_lines=False)
 Iris_virginica = dataframe[dataframe.species == "Iris_virginica"]
@@ -64,5 +127,6 @@ plt.savefig("Petal vs Sepal length.png")
 #print(__name__)
 if __name__ == '__main__':
     if len(sys.argv) < 2:
-        print ('You failed to provide Celsius degrees as input ')
+        print ('You failed to provide proper input ')
     sys.exit(1)  # abort because of error
+
